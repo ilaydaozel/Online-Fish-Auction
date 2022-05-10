d083b8fcb238d9b4241f5b4cf655beaf07330598
Çağdai <cag_das98@hotmail.com>
2022-05-10 23:33:51 +0300

    add fetch

--- src/components/AuctionCardList.jsx
+++ src/components/AuctionCardList.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 import { AuctionArray } from "../data"
 import AuctionCard from "./AuctionCard"
-import {mobile} from "../responsive"
-import React from 'react';
+import { mobile } from "../responsive"
+// import React from 'react';
+import React, { useState, useEffect } from "react";
+
 
 
 const Container = styled.div`
@@ -12,7 +14,7 @@ const CardContainer = styled.div`
     padding: 30px 100px;
     flex-wrap: wrap;
     justify-content: center;
-    ${mobile({padding: "10px", flexDirection: "column"})};
+    ${mobile({ padding: "10px", flexDirection: "column" })};
 `
 const Title = styled.h2`
     font-weight: 700;
@@ -20,22 +22,99 @@ const Title = styled.h2`
     text-align: center;
     margin-top: 80px;
     margin-bottom: 40px;
-    ${mobile({marginTop: "50px", marginBottom: "10px"})};
+    ${mobile({ marginTop: "50px", marginBottom: "10px" })};
 `
 
+// function AuctionCardList() {
+//     const [error, setError] = useState(null);
+//     const [isLoaded, setIsLoaded] = useState(false);
+//     const [auctionList, setAuctionList] = useState([]);
+//     const [userList, setUserList] = useState([]);
+
+//     useEffect(() => {
+//         fetch("/auction")
+//             .then(res => res.json())
+//             .then(
+//                 (result) => {
+//                     setIsLoaded(true);
+//                     setAuctionList(result);
+
+//                 },
+//                 (error) => {
+//                     console.log(error);
+//                     setIsLoaded(true);
+//                     setError(error);
+//                 })
+//     }, []);
+
+//     console.log(auctionList);
+
+//     return (
+//         <Container>
+//             <Title> Güncel Mezatlar </Title>
+//             <CardContainer>
+//                 {
+//                     auctionList.map((auction, i) => (
+//                         <li key={i}>
+//                             {auctionList.auctiunStatus}
+//                         </li>
+//                     ))
+//                 }
+//                 {AuctionArray.map(item => (
+//                     <AuctionCard item={item} />
+//                 ))}
+//             </CardContainer>
+
+//         </Container>
+//     )
+// }
+
 const AuctionCardList = () => {
- console.log("list:", AuctionArray)
-  return (
-    <Container>
-        <Title> Güncel Mezatlar </Title>
-        <CardContainer>
-            {AuctionArray.map(item => (
-                    <AuctionCard item={item}/>
-            ))}
-        </CardContainer>
-        
-    </Container>
-  )
+    // console.log("list:", AuctionArray)
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [auctionList, setAuctionList] = useState([]);
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        console.log("fetch");
+        fetch('http://localhost:8080/auction')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setAuctionList(result);
+                    console.log("res", result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                    console.log(error);
+                })
+    }, []);
+
+    console.log(auctionList);
+
+    return (
+        <Container>
+            <Title> Güncel Mezatlar </Title>
+            <CardContainer>
+                {
+                    auctionList.map((auction, i) => (
+                        <li key={i}>
+                            {auctionList.auctiounStatus}
+                        </li>
+                    ))
+                }
+                {AuctionArray.map(item => (
+                    <AuctionCard item={item} />
+                ))}
+            </CardContainer>
+
+        </Container>
+    )
 }
+
+
 
 export default AuctionCardList
--- src/components/FishForm.jsx
+++ src/components/FishForm.jsx
@@ -5,16 +5,48 @@ const FishForm = () => {
   const [kilogram, setkilogram] = useState('');
   const [floor_price, setfloor_price] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const new_fish = { species, kilogram, floor_price };
+
+    // ! lazım olcak saat dilimi de 
+    // const current = new Date();
+    // // const currentDateTime: current.toLocaleString();
+    // current.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    // console.log(current);
+    //!
+
+
+    fetch('http://localhost:8080/package', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        fishType: new_fish.species,
+        fishAmount: new_fish.kilogram,
+        sellerId: "12314123",
+        buyerId: null,
+        basePrice: new_fish.floor_price,
+        soldPrice: null,
+        soldDate: null,
+        auctionId: "1",
+        status: "UNSOLD"
+      }),
+    }).then((response) => response.json())
+      .then((result) => {
+        console.log("ekledi");
+      })
+  }
+
   return (
     <div className="create">
       <h1>Deniz ürünlerini sıradaki mezata ekle</h1>
-      <p style={{color: '#1b4171', fontWeight: 'bold'}}>Sıradaki mezat tarihi : 16.06.2022 16:00</p>
+      <p style={{ color: '#1b4171', fontWeight: 'bold' }}>Sıradaki mezat tarihi : 16.06.2022 16:00</p>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Balık Türü:</label>
-        <input 
-          type="text" 
-          required 
+        <input
+          type="text"
+          required
           value={species}
           onChange={(e) => setspecies(e.target.value)}
         />
@@ -31,11 +63,11 @@ const FishForm = () => {
           value={floor_price}
           onChange={(e) => setfloor_price(e.target.value)}
         />
-        
+
         <button>Balık Ekle</button>
       </form>
     </div>
   );
 }
- 
+
 export default FishForm;
